0f20944e5827fa7f99d36d6168539d673d24a79a
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-12-04 17:07:50 -0500

    created a div container around the header and main lements in order to target with css to center the content on the screen. polishing the js in order to enable clickable rows and added more notes and organized to explain capabilities of ag-grid

--- public/main.js
+++ public/main.js
@@ -65,32 +65,6 @@ const loading = () => {
                 GETTING DATA USING FETCH()
        ============================================== */
 
-
-    const columnDefs = [
-        {headerName: "Make", field: "make", sortable: true, filter: true },
-        {headerName: "Model", field: "model", sortable: true, filter: true },
-        {headerName: "Price", field: "price", sortable: true, filter: true }
-    ];
-
-    // specify the data
-    // let rowData = [
-    //   {make: "Toyota", model: "Celica", price: 35000},
-    //   {make: "Ford", model: "Mondeo", price: 32000},
-    //   {make: "Porsche", model: "Boxter", price: 72000}
-    // ];
-
-    // let the grid know which columns and what data to use
-    let gridOptions = {
-      columnDefs: columnDefs,
-    }
-
-    let gridContainer = document.querySelector('#myGrid')
-    console.log(gridContainer)
-
-    // create the grid passing in the div to use together with the columns & data we want to use
-    new agGrid.Grid(gridContainer, gridOptions);
-
-
     // Use fetch() to get data and then add that data to corresponding rows in grid (gridOptions)
     // rowData is created in the gridOpions Object
 
@@ -102,29 +76,44 @@ const loading = () => {
     });
 
 
+    let gridContainer = document.querySelector('#myGrid')
+    console.log(gridContainer)
+
     /*
+          ENABLE FILTERING AND SELECTING
 
-    ENABLE selection
+  // Filtering and Selecting the data columns are easy:
+      // const columnDefs = [
+      //     {headerName: "Make", field: "make", sortable: true, filter: true },
+      //     {headerName: "Model", field: "model", sortable: true, filter: true },
+      //     {headerName: "Price", field: "price", sortable: true, filter: true }
+      // ];
 
-    Being a programmer is a hectic job. Just when we thought that we are done with our assignment, the manager shows up with a fresh set of requirements! It turned out that we need to allow the user to select certain rows from the grid and to mark them as flagged in the system. We will leave the flag toggle state and persistence to the backend team. On our side, we should enable the selection and, afterwards, to obtain the selected records and pass them with an API call to a remote service endpoint.
+// With ag-grid, it is just a matter of adding and changing couple of properties:
 
-    */
-
-    // With ag-grid, it is just a matter of adding and changing couple of properties:
+        */
 
 
-    // specify the columns
-        // var columnDefs = [
-        //   {headerName: "Make", field: "make", checkboxSelection: true },
-        //   {headerName: "Model", field: "model"},
-        //   {headerName: "Price", field: "price"}
-        // ];
 
-    // let the grid know which columns and what data to use
-        // var gridOptions = {
-        //   columnDefs: columnDefs,
-        //   rowSelection: 'multiple'
-        // };
+
+        // specify the columns
+            let columnDefs = [
+              {headerName: "Make", field: "make", checkboxSelection: true },
+              {headerName: "Model", field: "model"},
+              {headerName: "Price", field: "price"}
+            ];
+
+        // let the grid know which columns and what data to use
+            let gridOptions = {
+              columnDefs: columnDefs,
+              rowSelection: 'multiple'
+            };
+
+
+
+    // create the grid passing in the div to use together with the columns & data we want to use
+    new agGrid.Grid(gridContainer, gridOptions);
+
 
 
         const filterBtn = document.querySelector('button')
